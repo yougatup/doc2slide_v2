6cc12aadad006566f846604bb91cffa7efa5f01e
hgshin <yougatup@gmail.com>
2021-09-09 22:38:31 +0900

    commit

--- slideInterface/script.js
+++ slideInterface/script.js
@@ -1,7 +1,7 @@
 var MAX_ELEMENTS_PER_SLIDE = 4;
 var SLIDES_PER_MIN = 2;
 
-var API_URL= 'http://server.hyungyu.com:5713/'
+var API_URL= 'https://server.hyungyu.com:5713/'
 var rowSelected = false;
 var selectedSlideIndex = -1;
 var selectedResourceIndex = -1;
